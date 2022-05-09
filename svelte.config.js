ba07cbe4336831d4857a816b3768118e76eb5758
Pronoun <pronoun.nationstates@gmail.com>
2022-05-09 11:12:54 -0700

    Fix build

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,15 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
+			// default options are shown
 			pages: 'docs',
 			assets: 'docs'
-		})
+		}),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true,
+			onError: 'continue'
+		}
 	}
 };
 

2f9887ef4bbf8bde5fd5456211e33036514fa265
Pronoun <pronoun.nationstates@gmail.com>
2024-01-14 01:05:39 -0800

    feat: add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['"Public Sans Variable"', ...defaultTheme.fontFamily.sans],
+				handwriting: ['"Kaushan Script"', "cursive"],
+			},
 			colors: {
 				brand: {
 					50: "#eeeffe",
